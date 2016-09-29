cbd2d348f1e37fa05e6a14c5491dfce004a21241
Brett Newman <brett.newman@gmail.com>
2016-09-28 23:36:08 -0400

    Removed dummy css theme, fixed issue with source maps not loading correctly

--- src/DoobesBackup.UI/app/components/app.component.ts
+++ src/DoobesBackup.UI/app/components/app.component.ts
@@ -5,39 +5,34 @@
     template: `
 <div class="row">
     <div class="col-md-3 left_col">
-        <div class="left_col scroll-view">
+        <div class="scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-                <a routerLink="/" class="site_title"><i class="fa fa-paw"></i> <span>{{title}}</span></a>
+                <a routerLink="/" class="site_title"><h1>{{title}}</h1></a>
             </div>
             <div class="clearfix"></div>
-
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
                 <div class="menu_section active">
-                    <h3>Main Menu</h3>
                     <main-menu></main-menu>
                 </div>
             </div>
         </div>
     </div>
-
-    <div class="top_nav">
-        <div class="nav_menu">
-            <nav class="" role="navigation">
-                <div class="nav toggle">
-                    <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-                </div>
-            </nav>
+    <div class="right_col col-md-9" role="main">
+        <div class="top-nav">
+            <div class="nav-menu">
+                <nav class="" role="navigation">
+                    <div class="nav toggle">
+                        <a id="menu-toggle"><i class="fa fa-bars"></i></a>
+                    </div>
+                </nav>
+            </div>
         </div>
-    </div>
-
-    <div class="right_col" role="main">
         <router-outlet></router-outlet>
     </div>
 </div>
-
 `
 })
 
 export class AppComponent {
-    title: string = "Doobes Backup Boom";
+    title: string = "Doobes Backup";
 }
